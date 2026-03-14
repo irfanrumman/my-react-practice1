e52905d009d934b67ba4de5c3c38942acd391da8
irfanrumman <mdirfansayid@gmail.com>
2026-03-14 07:24:56 +0600

    I have created note form and note box using react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import BioData from "./components/BioData";
-
-// event handeller
+import NoteForm from "./components/NoteForm";
 
 function App() {
-  const [dynamiccounter, setdynamiccouner] = useState(0);
-  const Increase = () => {
-    setdynamiccouner(dynamiccounter + 1);
-  };
-  const Decrease = () => {
-    if (dynamiccounter > 0) {
-      setdynamiccouner(dynamiccounter - 1);
-    }
-  };
-  return (
-    <>
-      <div>
-        <h2> The coun value is: {dynamiccounter}</h2>
-        <button onClick={Increase}>Increase By 1</button>
-        <button onClick={Decrease}>Decrease By 1</button>
-      </div>
-    </>
-  );
+  return <NoteForm />;
 }
 
 export default App;
---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "../css/NoteForm.css";
+
+const NoteForm = () => {
+  const [noteTitel, setNoteTitel] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editableNote, setEditableNote] = useState(null);
+  const changeTitelHandeler = (e) => {
+    setNoteTitel(e.target.value);
+  };
+  const submitHandeler = (e) => {
+    e.preventDefault();
+    if (noteTitel.trim() === "") {
+      return alert(`Please write here something...`);
+    }
+    editMode ? updateHandler() : createHandler();
+  };
+  const createHandler = () => {
+    const newNote = {
+      id: Date.now() + "",
+      titel: noteTitel,
+    };
+    setNotes([newNote, ...notes]);
+    setNoteTitel("");
+  };
+  const removeHandeler = (noteId) => {
+    setNotes(notes.filter((note) => note.id !== noteId));
+  };
+  const editHandeler = (noteItem) => {
+    setEditMode(true);
+    setEditableNote(noteItem);
+    setNoteTitel(noteItem.titel);
+  };
+
+  const updateHandler = () => {
+    const updatedNotes = notes.map((item) => {
+      if (editableNote.id == item.id) {
+        return { ...item, titel: noteTitel };
+      }
+      return item;
+    });
+    setNotes(updatedNotes);
+    setNoteTitel("");
+    setEditMode(false);
+  };
+  return (
+    <>
+      <div className="form">
+        <form onSubmit={submitHandeler}>
+          <h2>Add Your Note Here!</h2>
+          <input type="text" value={noteTitel} onChange={changeTitelHandeler} />
+          <button type="submit">{editMode ? "Update note" : "Add note"}</button>
+        </form>
+      </div>
+      <div className="notesArea">
+        <h3>All Notes is Here!</h3>
+        <ul>
+          {notes.map((noteItem) => (
+            <li key={noteItem.id}>
+              <span>{noteItem.titel}</span>
+              <button onClick={() => editHandeler(noteItem)}>Edit</button>
+              <button onClick={() => removeHandeler(noteItem.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default NoteForm;
